d2a7db0a42487f1940d1a91139134e0cf1bba489
adam <16adam61@gmail.com>
2021-04-14 02:54:00 -0700

    Added simple MDD generation. Mostly meant to be wireframe code, can be modified to make things easier if needed. Must add MDD merging next. Maybe move things into a class for that?

---
+++ mdd.py
@@ -0,0 +1,40 @@
+from typing import Dict, Tuple
+from single_agent_planner import move
+from collections import deque
+
+# Helper that returns a list of all reachable positions from a given location.
+def __next_locations(location):
+    return [move(location, i) for i in range(5)]
+
+# Pre:
+# Heuristics must be computed (use the one provided in the project)
+# Args:
+# start: Tuple containing the root location for the mdd.
+# heuristics: Heuristics provided as a dictionary lookup using locations.
+# Return:
+# A dictionary where locations are keys. Each entry contains the nodes
+# depth and children.
+# Description:
+# BFS search over the heuristic lookup.
+def gen_mdd(start: Tuple, heuristics: Dict) -> Dict:
+    mdd = {}
+    queue = deque([start])
+    depth = 0
+    curr_h = heuristics[start]
+    while queue:
+        currLoc = queue.popleft()
+
+        # Depth must increase for h to decrease. Because this is BFS, it will
+        # be strictly decreasing. So I used that to determine when depth changes.
+        if heuristics[currLoc] < curr_h:
+            depth += 1
+
+        mdd[currLoc] = {'depth': depth, 'children': []}
+        next_locs = __next_locations(currLoc)
+        for loc in next_locs:
+            if loc in heuristics:
+                if heuristics[loc] < heuristics[currLoc]:
+                    mdd[currLoc].append(loc)
+                    queue.append(loc)
+    return mdd
+
