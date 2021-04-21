e6d30b5b36e7c95b6062c0f9694872369fc8fad1
berkeliboz <berkeboz96@gmail.com>
2021-04-20 17:04:22 -0700

    Collision conflict classification added

--- mdd.py
+++ mdd.py
@@ -4,6 +4,7 @@
 
 
 ################################################################################
+from enum import Enum
 from typing import Dict, List, Tuple
 from single_agent_planner import move
 from collections import deque
@@ -14,9 +15,9 @@ from collections import deque
 class MDD_Node:
     def __init__(self, id: int, location: Tuple, depth: int) -> None:
         self.id = id
-        self.location = location # Tuple
-        self.depth = depth # Integer
-        self.children = [] # List containing children IDs
+        self.location = location  # Tuple
+        self.depth = depth  # Integer
+        self.children = []  # List containing children IDs
         pass
 
 
@@ -39,6 +40,7 @@ class MDD:
     # Returns a list of all the concrete children of the given node.
     def children_of(self, node: object) -> List:
         return [self.nodes[i] for i in node.children]
+
 
 # Helper that returns a list of all reachable positions from a given location.
 def __next_locations(location):
@@ -65,7 +67,7 @@ def __next_locations(location):
 # TODO: Add constraint handling
 def generate_mdd(start: Tuple, maxCost: int, heuristics: Dict) -> MDD:
     # Optimal path costs more than the requested cost
-    if maxCost > heuristics[start]:
+    if maxCost < heuristics[start]:
         return None
 
     number = 0
@@ -73,7 +75,7 @@ def generate_mdd(start: Tuple, maxCost: int, heuristics: Dict) -> MDD:
     node = MDD_Node(0, start, depth)
     mdd = MDD(node)
     queue = deque([number])
-    closedList = {} # Used to prevent duplicates
+    closedList = {}  # Used to prevent duplicates
     while queue:
         number = queue.popleft()
         node = mdd.nodes[number]
@@ -83,15 +85,15 @@ def generate_mdd(start: Tuple, maxCost: int, heuristics: Dict) -> MDD:
         nextLocs = __next_locations(currLoc)
         for loc in nextLocs:
             if loc in heuristics:
-                nextDepth = depth + 1 
-                nextVal = heuristics[loc] + nextDepth 
+                nextDepth = depth + 1
+                nextVal = heuristics[loc] + nextDepth
                 # If the depth + heuristic is greater than the requested cost, skip node.
                 # This is because heuristic decreases and depth increases. So this should
                 # always remain the same value, or increase. If max cost is much larger than
                 # the optimal path, it will lead to alot of extra wandering before proceeding
                 # to the goal.
                 if nextVal <= maxCost:
-                    if (loc, nextDepth) in closedList: # duplicate detected
+                    if (loc, nextDepth) in closedList:  # duplicate detected
                         # Just add duplicate as a child to the current node, and do not add to queue.
                         # Because this is a duplicate, the path is already calculated from this point on.
                         child = closedList[(loc, nextDepth)]
@@ -105,15 +107,45 @@ def generate_mdd(start: Tuple, maxCost: int, heuristics: Dict) -> MDD:
                         queue.append(child.id)
     return mdd
 
-def cross_nodes(agent_node : MDD_Node, other_node : MDD_Node, id : int) -> MDD_Node:
+
+class collision_type(Enum):
+    cardinal = 1
+    semi_cardinal = 2
+    non_cardinal = 3
+
+
+def cross_nodes(agent_node: MDD_Node, other_node: MDD_Node, id: int) -> MDD_Node:
     if agent_node.depth != other_node.depth:
         print("Can't cross nodes from different levels")
         return None
     if agent_node.location == other_node.location:
         return None
-    return MDD_Node(id, (agent_node,other_node),agent_node.depth)
+    return MDD_Node(id, (agent_node, other_node), agent_node.depth)
 
-def cross_mdds(agent_mdd : MDD, other_agent_mdd : MDD):
+
+#
+# {'a1': path_index, 'a2': other_path_index, 'loc': collision_information['loc'],
+# 'timestep': collision_information['timestep']}
+#
+
+def classify_collision(agent_mdd: MDD, other_agent_mdd: MDD, collision):
+    if agent_mdd is None or other_agent_mdd is None:
+        return None
+    loc = collision['loc'][0]
+
+    agent_node_filter = filter(lambda n: n.location == loc,agent_mdd.nodes)
+    agent_path = agent_node_filter.__sizeof__() > 1
+
+    other_agent_node_filter = filter(lambda n: n.location == loc,other_agent_mdd.nodes)
+    other_path = other_agent_node_filter.__sizeof__() > 1
+
+    if agent_path and other_path:
+        return collision_type.non_cardinal
+    elif agent_path is False and other_path is False:
+        return collision_type.cardinal
+    return collision_type.semi_cardinal
+
+def cross_mdds(agent_mdd: MDD, other_agent_mdd: MDD):
     cross_list = []
     number = 0
     agent_open = [agent_mdd.root]
@@ -146,4 +178,3 @@ def cross_mdds(agent_mdd : MDD, other_agent_mdd : MDD):
         other_agent_open = list(next_level_other_list.values())
 
     return cross_list
-
